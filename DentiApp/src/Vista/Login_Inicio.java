71d2d4144e6a1de11773c9b77d518d77be89917d
AntonioLr14 <antonioluzonruiz@gmail.com>
2023-10-20 14:45:03 +0200

    Update Login_Inicio.java

--- DentiApp/src/Vista/Login_Inicio.java
+++ DentiApp/src/Vista/Login_Inicio.java
@@ -11,6 +11,7 @@ import javax.swing.JButton;
 import javax.swing.JPasswordField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
 
 public class Login_Inicio extends JFrame {
 
@@ -52,21 +53,23 @@ public class Login_Inicio extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Usuario");
-		lblNewLabel.setBounds(168, 155, 55, 28);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblNewLabel.setBounds(168, 140, 75, 41);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Contraseña");
-		lblNewLabel_1.setBounds(168, 237, 75, 14);
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblNewLabel_1.setBounds(168, 237, 75, 41);
 		contentPane.add(lblNewLabel_1);
 		
 		textField = new JTextField();
-		textField.setBounds(280, 159, 176, 20);
+		textField.setBounds(280, 142, 176, 41);
 		
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(280, 234, 176, 20);
+		passwordField.setBounds(280, 234, 176, 41);
 		contentPane.add(passwordField);
 		
 		JButton btnNewButton = new JButton("Entrar");
@@ -82,13 +85,13 @@ public class Login_Inicio extends JFrame {
 				
 			}
 		});
-		btnNewButton.setBounds(367, 311, 89, 23);
+		btnNewButton.setBounds(395, 315, 97, 41);
 		contentPane.add(btnNewButton);
 		
 		
 		
 		JLabel lblNewLabel_2 = new JLabel("Reestablecer Contraseña");
-		lblNewLabel_2.setBounds(168, 315, 149, 14);
+		lblNewLabel_2.setBounds(182, 328, 149, 14);
 		contentPane.add(lblNewLabel_2);
 		
 	}
